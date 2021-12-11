4414e17952ea3b7998f12ed654ad412ca0bcacf5
5271135 <92792513+5271135@users.noreply.github.com>
2021-12-11 12:28:59 +0100

    Update and rename main.cpp to Update main.cpp

---
+++ Update main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "lib.h"
+
+using namespace std;
+
+int main() {
+    int dim;
+    cin >> dim;
+    float numeri[dim]
+    for (int i = 0; i < dim ; i++){
+        cin >> numeri[i];
+    }
+    numeroMassimo(numeri, dim);
+
+          cout << numeroMassimo(numeri, dim) << endl;
+
+    return 0;
+}
+

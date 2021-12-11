00fb26793ddbf98db63f073bbc21795bca9a2bf8
5271135 <92792513+5271135@users.noreply.github.com>
2021-12-11 14:35:12 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -2,9 +2,9 @@
 // Created by matte on 09/12/2021.
 //
 
-#ifndef UNTITLED15_LIB_H
-#define UNTITLED15_LIB_H
+#ifndef ES9_LIB_H
+#define ES9_LIB_H
 
 bool ricerca(char lista [10][20], char nome[1][20], int &pos);
 
-#endif // UNTITLED15_LIB_H
+#endif //ES9_LIB_H

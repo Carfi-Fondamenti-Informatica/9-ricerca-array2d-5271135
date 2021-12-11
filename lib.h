8860e63e88818a66892dce5b88c13be5d3e329a9
5271135 <92792513+5271135@users.noreply.github.com>
2021-12-11 13:10:49 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,6 +1,10 @@
-#ifndef UNTITLED17_LIB_H
-#define UNTITLED17_LIB_H
+//
+// Created by matte on 09/12/2021.
+//
+
+#ifndef UNTITLED15_LIB_H
+#define UNTITLED15_LIB_H
 
 bool ricerca(char lista [10][20], char nome[1][20], int &pos);
 
-#endif //UNTITLED17_LIB_H
+#endif // UNTITLED15_LIB_H
